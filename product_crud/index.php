fe7545d646ad7d0e78968fe682bf46363c54cdfa
Yousef <majhool.yf@gmail.com>
2021-06-20 02:24:28 -0700

    crud index is almost done

--- product_crud/index.php
+++ product_crud/index.php
@@ -19,9 +19,6 @@ try {
 } catch (PDOException $e) {
   echo "Query failed: " . $e->getMessage();
 }
-// echo "<pre>";
-// var_dump($products);
-// echo "</pre>";
 ?>
 <!doctype html>
 <html lang="en">
@@ -34,11 +31,11 @@ try {
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
-  <title>Hello, world!</title>
+  <title>Simple Products CRUD Review</title>
 </head>
 
 <body>
-  <h1>Hello, world!</h1>
+  <h1>Products list</h1>
 
   <!-- Optional JavaScript; choose one of the two! -->
 
@@ -50,6 +47,38 @@ try {
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
     -->
+
+<p> 
+<a href="create.php" type="button" class="btn btn-sm btn-outline-success">new product</a> 
+</p>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Image</th>
+        <th scope="col">Title</th>
+        <th scope="col">Price</th>
+        <th scope="col">Create Date</th>
+        <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($products as $i => $p) : ?>
+        <tr>
+          <th scope="row"><?php echo $i ?></th>
+          <td><?php echo $p['image']; ?></td>
+          <td><?php echo $p['title']; ?></td>
+          <td><?php echo $p['price']; ?>$</td>
+          <td><?php echo $p['create_date']; ?></td>
+          <td>
+            <button type="button" class="btn btn-sm btn-outline-danger">delete</button>
+            <button type="button" class="btn btn-sm btn-outline-warning">edit</button>
+          </td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+
 </body>
 
 </html>
